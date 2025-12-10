e4ead79d0e7cd90a32acbf71e7b316abd8509dc1
HugoRaimundo <vz4qpxbs45@privaterelay.appleid.com>
2025-12-10 20:38:36 -0300

    Initial Commit

---
+++ Echocast/EchocastApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchocastApp.swift
+//  Echocast
+//
+//  Created by actdigital on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct EchocastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
